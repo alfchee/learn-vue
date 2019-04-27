6949839afaf7326f0cf601efd3ac7e9c71c4596c
alfchee <alfchee@gmail.com>
2019-04-27 11:29:25 -0600

    Firts commit: Router + History Mode + Single File Vue Components + Global components

---
+++ real-world-vue/src/router.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventList from './views/EventList.vue'
+import EventCreate from './views/EventCreate.vue'
+import EventShow from './views/EventShow.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventList
+    },
+    {
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
+  ]
+})
