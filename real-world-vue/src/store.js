1b84242f1a424fed0af75414048a5a400cca7d17
alfchee <alfchee@gmail.com>
2019-04-28 20:04:07 -0600

    Adding acctions, getters and mutations to store

--- real-world-vue/src/store.js
+++ real-world-vue/src/store.js
@@ -1,10 +1,40 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      // create the event on backend service, and wait for completion
+      return EventService.postEvent(event).then(() => {
+        // once stored in DB, then continue with the dispatcher
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
